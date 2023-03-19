2d07ea4eb854e6ab13c63cd7e48e9eda7bb694cf
leocafo87 <leonardo.castellanos.forero@gmail.com>
2023-03-19 10:36:05 -0500

    Limpieza de datos

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,59 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    #df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
     #
     # Inserte su código aquí
     #
+    column_names = ["id","sexo","tipo_de_emprendimiento","idea_negocio","barrio","estrato","comuna_ciudadano","fecha_de_beneficio","monto_del_credito","línea_credito"]
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", skiprows=1,names=column_names)
+
+    #Elimina la col id
+    df = df.drop('id', axis=1)
+
+    #Convertir todo a minusculas
+    df = df.applymap(lambda s: s.lower() if type(s) == str else s)
+
+
+    #Limpiar Campo línea_credito
+    df['línea_credito'] = df['línea_credito'].str.replace('.','',regex=True)
+    df['línea_credito'] = df['línea_credito'].str.replace('-','',regex=True)
+    df['línea_credito'] = df['línea_credito'].str.replace('_','',regex=True)
+    df['línea_credito'] = df['línea_credito'].str.replace(' ','',regex=True)
+
+    #Limpiar Campo idea de negocio
+    df['idea_negocio'] = df['idea_negocio'].str.replace('.','',regex=True)
+    df['idea_negocio'] = df['idea_negocio'].str.replace('-','',regex=True)
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_','',regex=True)
+    df['idea_negocio'] = df['idea_negocio'].str.replace(' ','',regex=True)
+
+    #Limpiar Campo tipo_de_emprendimiento
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace('.','',regex=True)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace('-','',regex=True)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace('_','',regex=True)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.replace(' ','',regex=True)
+
+    #Limpiar Campo barrio
+    df['barrio'] = df['barrio'].str.replace('.','',regex=True)
+    df['barrio'] = df['barrio'].str.replace('-','',regex=True)
+    df['barrio'] = df['barrio'].str.replace('_','',regex=True)
+    df['barrio'] = df['barrio'].str.replace(' ','',regex=True)
+
+    #Limpiar Campo monto_del_credito
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('$','',regex=True)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','',regex=True)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(' ','',regex=True)
+    df['monto_del_credito'] = df['monto_del_credito'].apply(lambda x: x[0:x.find('.')+1] if x.find('.') != -1 else x)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('.','',regex=True)
+
+    #Limpiar la fecha
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], infer_datetime_format=True)
+
+    #eliminar registos con campos vacios
+    df = df.dropna()
+
+    #Eliminamos duplicados
+    df = df.drop_duplicates()
 
     return df
